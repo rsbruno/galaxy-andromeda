b65a43667d715a73e6b9c8d9042438a6f59e0b87
rsbruno <bruno.roberto@sou.unifal-mg.edu.br>
2022-06-02 09:14:44 -0300

    feat:adjacencyMatrix

--- aedsIII/main.cpp
+++ aedsIII/main.cpp
@@ -8,18 +8,40 @@ using namespace std;
 int main(void)
 {
     ifstream myfile;
-    std::vector<std::string> out;
+    ifstream myfile2;
+    int maxLines = 0;
 
     myfile.open("att48.txt");
     if (myfile.is_open())
+        while (myfile.peek() != EOF)
+        {
+            string line;
+            getline(myfile, line);
+            maxLines++;
+        }
+
+    int adjacencyMatrix[maxLines][maxLines];
+
+    myfile2.open("att48.txt");
+    if (myfile2.is_open())
     {
         string mychar, index, posX, posY;
-        while (myfile)
+        int indexLine = 0;
+        while (myfile2)
         {
-            myfile >> index;
-            myfile >> posX;
-            myfile >> posY;
-            cout << index << ": " << posX << " - " << posY << endl;
+            int indexColumn = 0;
+            myfile2 >> index;
+            myfile2 >> posX;
+            myfile2 >> posY;
+
+            adjacencyMatrix[indexLine][indexColumn++] = stoi(index);
+            adjacencyMatrix[indexLine][indexColumn++] = stoi(posX);
+            adjacencyMatrix[indexLine++][indexColumn++] = stoi(posY);
         }
+    }
+
+    for (int i = 0; i < maxLines; i++)
+    {
+        cout << adjacencyMatrix[i][0] << ": " << adjacencyMatrix[i][1] << " " << adjacencyMatrix[i][2] << endl;
     }
 }
