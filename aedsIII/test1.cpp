bcde306bd4c472dbae5686e78974b3041ee663a7
rsbruno <bruno.roberto@sou.unifal-mg.edu.br>
2022-07-21 11:23:50 -0300

    FEAT: Printing the biggest path

--- aedsIII/test1.cpp
+++ aedsIII/test1.cpp
@@ -1,18 +1,25 @@
-// C++ program for Dijkstra's single source shortest path
-// algorithm.The program is for adjacency matrix
-// representation of the graph.
 #include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
-// Number of vertices in the graph
 #define V 9
+int maxPath[3] = {-1};
+queue<int> biggestParent;
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
+
+void print_queue(queue<int> q)
+{
+    queue<int> temp = q;
+    while (!temp.empty()) {
+        cout << temp.front()<<" ";
+        temp.pop();
+    }
+    cout << '\n';
+}
+
+
 int minDistance(int dist[], bool sptSet[])
 {
-    // Initialize min value
     int min = INT_MAX, min_index;
     for (int i = 0; i < V; i++)
         if (sptSet[i] == false && dist[i] <= min)
@@ -20,88 +27,72 @@ int minDistance(int dist[], bool sptSet[])
     return min_index;
 }
 
-// Function to print shortest path from source to j using
-// parent array
 void printPath(int parent[], int j)
 {
-    // Base Case : If j is source
     if (parent[j] == -1)
         return;
     printPath(parent, parent[j]);
-    cout << j << " ";
+    biggestParent.push(j);
 }
 
-// A utility function to print the constructed distance
-// array
 void printSolution(int dist[], int n, int parent[], int src)
 {
-    cout << "Vertex\t Distance\tPath";
-    for (int i = 1; i < V; i++)
+    int aux;
+    for (int i = 0; i < V; i++)
     {
-    
-            printf("\n%d -> %d \t\t %d\t\t%d ", src, i, dist[i], src);
+        if (dist[i] > maxPath[2])
+        {
+            queue<int> empty;
+            swap(biggestParent, empty);
+            maxPath[0] = src;
+            maxPath[1] = i;
+            maxPath[2] = dist[i];
+            biggestParent.push(src);
             printPath(parent, i);
-      
+        }
+        
     }
-    cout << endl;
 }
 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
 void dijkstra(int graph[V][V], int src)
 {
-    // The output array. dist[i] will hold the shortest
-    // distance from src to i
     int dist[V];
-
-    // sptSet[i] will true if vertex i is included / in
-    // shortest path tree or shortest distance from src to i
-    // is finalized
     bool sptSet[V] = {false};
+    int parent[V];
 
-    // Parent array to store shortest path tree
-    int parent[V] = {-1};
+    for (int k = 0; k < V; k++)
+        parent[k] = -1;
 
-    // Initialize all distances as INFINITE
     for (int i = 0; i < V; i++)
         dist[i] = INT_MAX;
 
-    // Distance of source vertex from itself is always 0
     dist[src] = 0;
 
-    // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++)
+    for (int count = 0; count < V; count++)
     {
-        // Pick the minimum distance vertex from the set of
-        // vertices not yet processed. u is always equal to
-        // src in first iteration.
         int u = minDistance(dist, sptSet);
-        // Mark the picked vertex as processed
         sptSet[u] = true;
-        // Update dist value of the adjacent vertices of the
-        // picked vertex.
         for (int v = 0; v < V; v++)
-            // Update dist[v] only if is not in sptSet,
-            // there is an edge from u to v, and total
-            // weight of path from src to v through u is
-            // smaller than current value of dist[v]
+        {
             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
             {
-                parent[v] = u;
-                cout<<u;
+                if (v == u)
+                {
+                    parent[v] = -1;
+                }
+                else
+                {
+                    parent[v] = u;
+                }
                 dist[v] = dist[u] + graph[u][v];
             }
-            cout<<"teste"<<endl;
+        }
     }
-    // print the constructed distance array
     printSolution(dist, V, parent, src);
 }
 
-// Driver Code
 int main()
 {
-    // Let us create the example graph discussed above
     int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
                        {4, 0, 8, 0, 0, 0, 0, 11, 0},
                        {0, 8, 0, 7, 0, 4, 0, 0, 2},
@@ -111,6 +102,13 @@ int main()
                        {0, 0, 0, 14, 0, 2, 0, 1, 6},
                        {8, 11, 0, 0, 0, 0, 1, 0, 7},
                        {0, 0, 2, 0, 0, 0, 6, 7, 0}};
-    dijkstra(graph, 0);
+    for (int i = 0; i < V; i++)
+    {
+        dijkstra(graph, i);
+    }
+
+    cout << "nome-arquivo" << endl;
+    print_queue(biggestParent);
+    cout << maxPath[2] << endl;
     return 0;
 }
